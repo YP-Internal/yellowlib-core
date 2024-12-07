a7e05bad0df3bbeafc6ae6a762f828178dc88c99
RotcivOcnarb <rotciv_ocnarb@hotmail.com>
2024-12-07 14:10:17 -0300

    attribute editor fix

--- Attributes/Editor/SceneAttributeDrawer.cs
+++ Attributes/Editor/SceneAttributeDrawer.cs
@@ -3,6 +3,7 @@ using UnityEditor;
 using UnityEngine;
 
 namespace YellowPanda.Editor {
+#if UNITY_EDITOR
     [CustomPropertyDrawer(typeof(SceneAttribute))]
     public class SceneAttributeDrawer : PropertyDrawer {
         public override void OnGUI(Rect position, SerializedProperty property, GUIContent label) {
@@ -10,4 +11,5 @@ namespace YellowPanda.Editor {
             property.serializedObject.ApplyModifiedProperties();
         }
     }
+#endif
 }
