4f2907289b83d4564ca3dddf16546438291f0ac6
Murilo Bezerra <murilob@yellowpanda.games>
2026-02-25 11:50:09 -0300

    Add asset creation

---
+++ Scripts/AssetCreation/OverridableVariable.cs
@@ -0,0 +1,42 @@
+using Sirenix.OdinInspector;
+using UnityEngine;
+
+namespace YellowPanda.Core.AssetCreation
+{
+    [System.Serializable]
+    public class OverridableVariable<DataClass, ScriptableObjectClass> where DataClass : class where ScriptableObjectClass : OverridableVariableSO<DataClass>
+    {
+        [SerializeField] bool useOverride = false;
+        [SerializeField, ShowIf("@useOverride")] DataClass overrideValue = null;
+        [SerializeField, ShowIf("@!useOverride"), InlineEditor] ScriptableObjectClass defaultValue = null;
+
+#if UNITY_EDITOR
+        [Button]
+        void CreateAsset()
+        {
+            string path = ScriptableObjectFactory.GetEditorAssetPath();
+            defaultValue = ScriptableObjectFactory.Create<ScriptableObjectClass>(path);
+        }
+#endif
+        public DataClass Value
+        {
+            get => useOverride ? overrideValue : defaultValue.Value;
+        }
+    }
+}
+
+
+public class OverridableVariableSO<DataClass> : ScriptableObject where DataClass : class
+{
+    public DataClass Value;
+}
+
+//Exemple usage:
+
+[System.Serializable]
+public class OverridableFloat
+{
+    public float value;
+}
+
+public class FloatOverridableSO : OverridableVariableSO<OverridableFloat> { }
---
+++ Scripts/AssetCreation/ScriptableObjectFactory.cs
@@ -0,0 +1,61 @@
+using System.IO;
+using UnityEngine;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+namespace YellowPanda.Core.AssetCreation
+{
+    public static class ScriptableObjectFactory
+    {
+        public static T Create<T>(string path) where T : ScriptableObject
+        {
+            T instance = ScriptableObject.CreateInstance<T>();
+
+            EnsureDirectoryExists(path);
+
+            AssetDatabase.CreateAsset(instance, path);
+            AssetDatabase.SaveAssets();
+
+            return instance;
+        }
+
+#if UNITY_EDITOR
+        public static string GetEditorAssetPath()
+        {
+            string directoryPath = "Assets/Resources/Settings";
+
+            EnsureDirectoryExists(directoryPath);
+
+            string path = EditorUtility.SaveFilePanelInProject(
+                "Create SO",
+                "NewData",
+                "asset",
+                "Enter asset name",
+                directoryPath
+            );
+
+            return path;
+        }
+#endif
+
+        public static void EnsureDirectoryExists(string filePath)
+        {
+            try
+            {
+                string directoryPath = Path.GetDirectoryName(filePath);
+
+                if (!Directory.Exists(directoryPath))
+                {
+                    Debug.Log($"Creating directory: {directoryPath}");
+                    Directory.CreateDirectory(directoryPath);
+                    AssetDatabase.Refresh();
+                }
+            }
+            catch (System.Exception ex)
+            {
+                Debug.LogError($"Failed to create directory for path: {filePath}\nError: {ex.Message}");
+            }
+        }
+    }
+}
